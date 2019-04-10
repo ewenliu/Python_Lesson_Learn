0d667946283afa0503f773414a0339e26bb45613
ewenliu <ewen.liu@outlook.com>
2019-04-10 21:32:10 +0800

    add pagination function

---
+++ Flask_Blog/9_Pagination/run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time          : 2019/4/7 15:11
+# @Author        : ewen.liu
+# @Department    :
+# @File          : flaskblog.py.py
+# @Function      : Code for Flask tutorial
+
+from flaskblog import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
