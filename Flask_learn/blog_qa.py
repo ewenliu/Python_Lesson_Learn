e3091f2551f061108e353ace750b628e158a4cda
ewenliu <ewen.liu@outlook.com>
2019-05-17 20:46:01 +0800

    add bootstrap js and navigation-bar

--- Flask_learn/blog_qa.py
+++ Flask_learn/blog_qa.py
@@ -4,15 +4,15 @@
 # @Department    :
 # @Function      :
 
-from flask import Flask
+from flask import Flask, render_template
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route('/')
-def hello_word():
-    return 'hello_word !'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
