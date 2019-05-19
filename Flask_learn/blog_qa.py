8373160a06946669d1519d5f7d004c2410395341
ewenliu <ewen.liu@outlook.com>
2019-05-19 16:19:42 +0800

    login page/css added

--- Flask_learn/blog_qa.py
+++ Flask_learn/blog_qa.py
@@ -4,7 +4,7 @@
 # @Department    :
 # @Function      :
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
 
 app = Flask(__name__)
@@ -13,6 +13,13 @@ app.config.from_object(config)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
 
 if __name__ == '__main__':
     app.run()
