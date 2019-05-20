12f086b9c79aee832769dd61384c39fbc4f91add
ewenliu <ewen.liu@outlook.com>
2019-05-20 15:00:43 +0800

    add regist logic

--- Flask_learn/blog_qa.py
+++ Flask_learn/blog_qa.py
@@ -4,11 +4,14 @@
 # @Department    :
 # @Function      :
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import config
+from models import User
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(config)
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -26,7 +29,25 @@ def regist():
     if request.method == 'GET':
         return render_template('regist.html')
     else:
-        pass
+        telephone = request.form.get('telephone')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password_confirm = request.form.get('password_confirm')
+
+        #Phone number verfication
+        user = User.query.filter(User.telephone == telephone).first()
+        if user:
+            return 'This phone number has been registered!'
+        else:
+            #password should equal to password_confirm
+            if password != password_confirm:
+                return 'Password is not equal to the password confirm!'
+            else:
+                user = User(telephone=telephone, username=username, password=password)
+                db.session.add(user)
+                db.session.commit()
+                #If registration successful, then skip to login page
+                return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
