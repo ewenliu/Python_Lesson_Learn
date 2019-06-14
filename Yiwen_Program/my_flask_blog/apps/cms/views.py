1fac4475f8b656e42682fe0154a195f7a291a5b7
ewenliu <ewen.liu@outlook.com>
2019-06-14 16:58:19 +0800

    add basic cms login html

--- Yiwen_Program/my_flask_blog/apps/cms/views.py
+++ Yiwen_Program/my_flask_blog/apps/cms/views.py
@@ -5,7 +5,7 @@
 # @File    : views.py
 # @Documents:
 
-from flask import Blueprint
+from flask import Blueprint, views, render_template
 
 bp = Blueprint('cms', __name__, url_prefix='/cms')
 
@@ -13,3 +13,16 @@ bp = Blueprint('cms', __name__, url_prefix='/cms')
 @bp.route('/')
 def index():
     return 'cms index'
+
+
+class LoginView(views.MethodView):
+
+    @staticmethod
+    def get():
+        return render_template('cms/cms_login.html')
+
+    def post(self):
+        pass
+
+
+bp.add_url_rule('/login/', view_func=LoginView.as_view('login'))
