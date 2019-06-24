f385a5c158c74dac2eb0ad252d925f9edd97049c
ewenliu <ewen.liu@outlook.com>
2019-06-24 11:58:23 +0800

    add captcha-img/click change captcha-img

--- Yiwen_Program/my_flask_blog/apps/front/views.py
+++ Yiwen_Program/my_flask_blog/apps/front/views.py
@@ -8,16 +8,29 @@
 from flask import (
     Blueprint,
     views,
-    render_template
+    render_template,
+    make_response
 )
+from io import BytesIO
+from utils.captcha import Captcha
 
 bp = Blueprint('front', __name__)
 
 
 @bp.route('/')
 def index():
-    return 'front index'\
+    return 'front index'
 
+
+@bp.route('/captcha/')
+def graph_captcha():
+    text, image = Captcha.gene_graph_captcha()
+    out = BytesIO()
+    image.save(out, 'png')
+    out.seek(0)
+    resp = make_response(out.read())
+    resp.content_type = 'image/png'
+    return resp
 
 
 class SignupView(views.MethodView):
