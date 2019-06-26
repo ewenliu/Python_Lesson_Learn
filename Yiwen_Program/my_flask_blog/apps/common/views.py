726b56433651228b979045dc5c79296a2c76d46b
ewenliu <ewen.liu@outlook.com>
2019-06-26 17:55:39 +0800

    cache sms/img captcha to memcached

--- Yiwen_Program/my_flask_blog/apps/common/views.py
+++ Yiwen_Program/my_flask_blog/apps/common/views.py
@@ -4,10 +4,17 @@
 # @Email   : ewen.liu@outlook.com
 # @File    : views.py
 # @Documents:
-from flask import Blueprint, request
-from utils import restful, smssender
+from flask import (
+    Blueprint,
+    request,
+    make_response
+)
+from utils import restful, smssender, blog_cache
 from utils.captcha import Captcha
 from .forms import SMSCaptchaForm
+from io import BytesIO
+
+
 
 bp = Blueprint('common', __name__, url_prefix='/c')
 
@@ -35,8 +42,22 @@ def sms_captcha():
         telephone = form.telephone.data
         captcha = Captcha.gene_text(number=4)
         if smssender.send(telephone, captcha=captcha):
+            blog_cache.set(telephone, captcha)
             return restful.success()
         else:
             return restful.params_error(message='短信验证码发送失败')
     else:
         return restful.params_error(message='参数错误！')
+
+
+@bp.route('/captcha/')
+def graph_captcha():
+    text, image = Captcha.gene_graph_captcha()
+    blog_cache.set(text.lower(), text.lower())
+    out = BytesIO()
+    image.save(out, 'png')
+    out.seek(0)
+    resp = make_response(out.read())
+    resp.content_type = 'image/png'
+    return resp
+
--- Yiwen_Program/my_flask_blog/apps/front/views.py
+++ Yiwen_Program/my_flask_blog/apps/front/views.py
@@ -8,11 +8,9 @@
 from flask import (
     Blueprint,
     views,
-    render_template,
-    make_response
+    render_template
 )
-from io import BytesIO
-from utils.captcha import Captcha
+
 
 bp = Blueprint('front', __name__)
 
@@ -20,17 +18,6 @@ bp = Blueprint('front', __name__)
 @bp.route('/')
 def index():
     return 'front index'
-
-
-@bp.route('/captcha/')
-def graph_captcha():
-    text, image = Captcha.gene_graph_captcha()
-    out = BytesIO()
-    image.save(out, 'png')
-    out.seek(0)
-    resp = make_response(out.read())
-    resp.content_type = 'image/png'
-    return resp
 
 
 class SignupView(views.MethodView):
